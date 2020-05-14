534ea4367497187da863c0a382e64e32ed14052c
Gustavo Barbosa <gusbdev@gmail.com>
2020-05-14 20:57:55 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bet Controller - Em desenvolvimento</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#77dd77',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
